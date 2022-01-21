e1b65da6f4b3eadf6a5b89d3d1cda4410de352f7
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-01-20 16:38:39 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+    const messageArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+        messageArray.push(message);
+    };
+    // console.log(messageArray);
+    return messageArray;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(count) {
+    while (count > -1) {
+        console.log(count);
+        count--;
+    }
+}
+countDown(10);
