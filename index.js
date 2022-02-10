456ed6bcecb1a4ced64486cbad4eff560a08a5f0
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-02-10 00:50:50 -0800

    commit tree da6e881966c2bb5164c85747fa62f1434d456fd7

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+
+// function writeCards(arr, event) {
+//     const messageArray = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         const message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+//         messageArray.push(message);
+//     };
+//     // console.log(messageArray);
+//     return messageArray;
+// }
+// writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+
+const messageArray = [];
 function writeCards(arr, event) {
-    const messageArray = [];
 
     for (let i = 0; i < arr.length; i++) {
-        const message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
-        messageArray.push(message);
+        messageArray.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
     };
-    // console.log(messageArray);
     return messageArray;
 }
 writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
 
 
-function countDown(count) {
-    while (count > -1) {
-        console.log(count);
-        count--;
-    }
-}
-countDown(10);
+
+
+// function countDown(count) {
+//     while (count > -1) {
+//         console.log(count);
+//         count--;
+//     }
+// }
+// countDown(10);
